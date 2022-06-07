655e89145bb7340cbbd9d07cf5f1f7c16bf566bc
Rohin <kotianra@mail.uc.edu>
2022-06-07 12:26:46 -0400

    Share Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class
+Main
+{
+    public static void main(String[] args)
+    {
+        int numberOfKids = 3;
+        String isItRaining = "yes";
+        double gasPrice = 4.56;
+        int favNumber = 27;
+        double shoeSize = 8.5;
+        int birthMonth = 10;
+        String fullName = "Rohin Anil Kotian";
     }
 }
